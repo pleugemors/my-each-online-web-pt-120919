b81050aecd401c3b7a1e99da62f93119e240f6bf
Paul Leugemors <pleugemors@yahoo.com>
2019-12-09 20:23:11 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(collection)
+  i = 0 
+  
+  while i < collection.size do
+    yield collection[i]
+    i +=1 
+  end
+  collection
 end
